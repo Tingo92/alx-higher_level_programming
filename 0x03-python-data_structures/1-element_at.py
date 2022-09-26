5ddd04f207c3d6f57afb9bc6ce8e3d710bdf9791
Tingo92 <ngaraimba@gmail.com>
2022-09-26 13:26:30 -0700

    List

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def element_at(my_list, idx):
+    match point:
+        case (my_list, <0):
+            print("None")
+        case(my_list, >len(my_list)):
+            print("None")
+        case(my_list, idx):
+            print("Element at index {:d} is {}".format(idx, element_at(my_list, idx)))
