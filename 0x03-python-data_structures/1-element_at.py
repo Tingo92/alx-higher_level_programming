1f3af8d6513857c409ac55947bd74cecf6430e04
Tingo92 <ngaraimba@gmail.com>
2022-09-26 13:41:34 -0700

    more files

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
+
+
 def element_at(my_list, idx):
-    match point:
-        case (my_list, <0):
-            print("None")
-        case(my_list, >len(my_list)):
-            print("None")
-        case(my_list, idx):
-            print("Element at index {:d} is {}".format(my_list[idx]))
+    if (idx < 0) or (idx > len(my_list) - 1):
+        return None
+    return(my_list[idx])
