7efc7f1ae3b0936d42aee9dd9b6965c89c42c51c
Tingo92 <ngaraimba@gmail.com>
2022-12-01 08:50:59 -0800

    table with attributes

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,3 +1,3 @@
 -- Creating a table in mysql database
 
-CREATE TABLE IF NOT EXISTS <first_table>(<id><INT>, <name><VARCHAR(256)>);
+CREATE TABLE IF NOT EXISTS `first_table` (`id` INT, `name` VARCHAR(256));
